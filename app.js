c3ed5b9c456cc56e443beac68da48c8c76046eba
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-02 20:55:29 +0530

    final release 1.1.2

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ app.use(bodyParser.json()); // application/json
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         'Access-Control-Allow-Methods',
-//         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-//     );
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
